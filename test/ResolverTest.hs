ef8c249baf51ff17f6ecde566c74c741996ac8e5
Reed Oei <oei.reed@gmail.com>
2018-04-25 16:23:02 -0500

    Added struct size calculationg test.

--- test/ResolverTest.hs
+++ test/ResolverTest.hs
@@ -11,6 +11,7 @@ import Compiler.Types
 
 asteroidStruct = StructDef "Asteroid" [Var (Type Value (NamedType "int")) "yx",Var (Type Value (NamedType "int")) "points"]
 asteroidMapStruct = StructDef "AsteroidMap" [Var (Type Value (NamedType "unsigned int")) "length",Var (Array 50 (Type Value (NamedType "Asteroid"))) "asteroids"]
+canvasStruct = StructDef "Canvas" [Var (Type Value (NamedType "unsigned int")) "height",Var (Type Value (NamedType "unsigned int")) "width",Var (Type Value (NamedType "unsigned char")) "pattern",Var (Type Pointer (Type Pointer (NamedType "char"))) "canvas"]
 
 testEnv = set file (CFile "test.c" [asteroidStruct, asteroidMapStruct]) emptyEnvironment
 
@@ -25,4 +26,7 @@ resolverTests =
             let (offset, _) = runState (structOffset asteroidStruct "points") testEnv
 
             offset `shouldBe` 4
+
+        it "accounts for padding in the struct when calculating the size" $
+            sizeof (StructType canvasStruct) `shouldBe` 16
 
